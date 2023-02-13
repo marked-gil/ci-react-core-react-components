25c50a450e75cc9a9ac3dcc14cc1935bafbcaede
marked-gil <markgil_creates@yahoo.com>
2023-02-13 16:06:12 +0000

    Add classCounter.js

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import FunctionalGreeting from "./components/FunctionalGreeting";
 import StatefulGreeting from "./components/StatefulGreeting";
 import HooksCounter from "./components/HooksCounter";
+import ClassCounter from './components/ClassCounter';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting name="John" />
+      {/* <FunctionalGreeting name="John" /> */}
       {/* <StatefulGreeting name="Anna" /> */}
-      {/* <HooksCounter name="Mike" /> */}
+      <ClassCounter />
+      <HooksCounter name="Mike" />
     </div>
   );
 }
